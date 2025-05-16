08573709731a4923ff443f3d82bdb0561bdb1ef5
Eva Maria Veitmaa <e.m.veitmaa@gmail.com>
2025-05-16 20:35:32 +0300

    Remove extra title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import SlotMachine from './components/SlotMachine';
 function App() {
   return (
     <div className="App">
-      <h1>React Slot Machine</h1>
       <SlotMachine />
     </div>
   );
